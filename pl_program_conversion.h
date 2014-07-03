5e97366bda97b2ddb51a84d60d53e792328f278f
Fabian Kliebhan <fabian.kliebhan@gmail.com>
2014-07-03 20:49:15 +0200

    inital commit

---
+++ pl_program_conversion.h
@@ -0,0 +1,26 @@
+#include "plpgsql.h"
+#include "nodes/pg_list.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <igraph/igraph.h>
+
+
+#define MAXDOTFILESIZE 4096
+
+#define eos(s) ((s)+strlen(s))
+
+
+
+
+/**********************************************************************
+ * Function declarations
+ **********************************************************************/
+
+
+/* ----------
+ * Functions in pl_program_conversion.c
+ * ----------
+ */
+void analyse(PLpgSQL_execstate *estate, PLpgSQL_function *func);
+
